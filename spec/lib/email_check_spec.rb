ccab5bc88fd5c2a274c9f554ec1a164c263d7ac0
kazuki.hattori <kazuki.hattori@mixi.co.jp>
2017-04-14 16:18:43 +0900

    add check_spec

--- spec/lib/email_check_spec.rb
+++ spec/lib/email_check_spec.rb
@@ -3,5 +3,36 @@ require 'email_check'
 
 describe EmailCheck do
 
+  it '正しく動く' do
+    input = StringIO.new(<<-EOF
+  jfkdsljfls@example.com
+  fhudsihf@example.com
+  ijfiofheruh@example.com
+  jfkdsdjksfjklsd@example.com
+  fjkfldjslfdj8s9fus@example.com
+  9420383290u@example.com
+  @example.com
+  ddds.@example.com
+  fdskh..fds@example.com
+  fdsjfkl@ewr@example.com
+  EOF
+    )
+    EmailCheck.check(input, output)
+    answer = <<-EOF
+  ok
+  ok
+  ok
+  ok
+  ok
+  ok
+  ng
+  ng
+  ng
+  ng
+  EOF
+    expect(output.read).to eq answer
+
 end
 
+
+end
