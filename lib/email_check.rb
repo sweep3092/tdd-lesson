9070e407d095badf5c1fa4b143f451ab3046fab3
You Kobayashi <sweep.3092@gmail.com>
2017-04-14 16:35:40 +0900

    refactor email_check

--- lib/email_check.rb
+++ lib/email_check.rb
@@ -1,2 +1,14 @@
 class EmailCheck
+  class << self
+    def check(input, output)
+      while oneline = input.gets
+        output.puts(valid_email?(oneline) ? 'ok' : 'ng')
+      end
+    end
+
+    def valid_email?(email)
+      # TODO: emailがただしいのか
+    end
+  end
+
 end
--- spec/lib/email_check_spec.rb
+++ spec/lib/email_check_spec.rb
@@ -3,36 +3,39 @@ require 'email_check'
 
 describe EmailCheck do
 
-  it '正しく動く' do
-    input = StringIO.new(<<-EOF
-  jfkdsljfls@example.com
-  fhudsihf@example.com
-  ijfiofheruh@example.com
-  jfkdsdjksfjklsd@example.com
-  fjkfldjslfdj8s9fus@example.com
-  9420383290u@example.com
-  @example.com
-  ddds.@example.com
-  fdskh..fds@example.com
-  fdsjfkl@ewr@example.com
-  EOF
-    )
+  shared_context '正しく動く' do
+    input = StringIO.new(emails.join("\n"))
+    output = StringIO.new
+
     EmailCheck.check(input, output)
-    answer = <<-EOF
-  ok
-  ok
-  ok
-  ok
-  ok
-  ok
-  ng
-  ng
-  ng
-  ng
-  EOF
+
     expect(output.read).to eq answer
+  end
 
+  describe '.check' do
+    context 'with valid emails' do
+      emails = %w(
+jfkdsljfls@example.com
+fhudsihf@example.com
+ijfiofheruh@example.com
+jfkdsdjksfjklsd@example.com
+fjkfldjslfdj8s9fus@example.com
+9420383290u@example.com
+      )
+      answer = emails.map{'ok'}
+
+      include_context '正しく動く'
+    end
+
+    context 'with .. or . emails' do
+      emails = %w(
+ddds.@example.com
+fdskh..fds@example.com
+      )
+      answer = emails.map{'ng'}
+
+      include_context '正しく動く'
+    end
+  end
 end
 
-
-end
