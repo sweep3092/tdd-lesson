3b1a0c36788406602c245b98a95cba8d8e636fa8
kazuki.hattori <kazuki.hattori@mixi.co.jp>
2017-04-14 11:17:10 +0900

    add top_spec

--- spec/lib/stack_spec.rb
+++ spec/lib/stack_spec.rb
@@ -2,8 +2,22 @@ require 'spec_helper'
 require 'stack'
 
 describe Stack do
-  it "create a stack" do
+
+  before do
     stack = Stack.new
-    expect(stack.empty?).to be_truthy
   end
+  
+  describe '#empty?' do
+    it "to be true when create a stack" do
+      expect(stack.empty?).to be_truthy
+    end
+  end
+
+  describe '#top' do
+    it "when create a stack" do
+      expect(stack.top).to eq 1
+    end
+  end
+
+
 end
