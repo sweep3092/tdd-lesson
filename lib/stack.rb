cd31a45585124a408070f6dbcb92e3dc68741758
You Kobayashi <sweep.3092@gmail.com>
2017-04-14 11:37:59 +0900

    refactor, add size spec

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,6 +1,22 @@
 class Stack
+  def initialize
+  end
+
   def empty?
     true
   end
+
+  def push(v)
+    @value = v
+  end
+
+  def top
+    @value
+  end
+
+  def size
+    1
+  end
+
 end
 
--- spec/lib/stack_spec.rb
+++ spec/lib/stack_spec.rb
@@ -2,22 +2,32 @@ require 'spec_helper'
 require 'stack'
 
 describe Stack do
-
-  before do
-    stack = Stack.new
-  end
-  
   describe '#empty?' do
-    it "to be true when create a stack" do
-      expect(stack.empty?).to be_truthy
+    context 'when create a stack' do
+      it "to be true" do
+        stack = Stack.new
+        expect(stack.empty?).to be_truthy
+      end
     end
   end
 
   describe '#top' do
-    it "when create a stack" do
-      expect(stack.top).to eq 1
+    context 'when 1 pushed' do
+      it "to be 1" do
+        stack = Stack.new
+        stack.push(1)
+        expect(stack.top).to eq 1
+      end
     end
   end
 
-
+  describe '#size' do
+    context 'when a value pushed' do
+      it "to be 1" do
+        stack = Stack.new
+        stack.push(1)
+        expect(stack.size).to eq 1
+      end
+    end
+  end
 end
